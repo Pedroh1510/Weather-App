45ae55c92fb0b522325d5c98793e8db8c01933fc
Pedro <pedroh1510@hotmail.com>
2020-03-14 22:14:51 -0300

    Updates

---
+++ precache-manifest.a0103f709244ebfe5a78b78aacf01ea2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fd51692b0c1b35c2427bcc2511cb1c",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "a8c6fba2d172be6a16b3",
+    "url": "/Weather-App/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cc53a1fe035ed6aba8c1",
+    "url": "/Weather-App/static/js/2.82337c3c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Weather-App/static/js/2.82337c3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8c6fba2d172be6a16b3",
+    "url": "/Weather-App/static/js/main.a99ffe90.chunk.js"
+  },
+  {
+    "revision": "6160a6e9a5bd4f3d8bb2",
+    "url": "/Weather-App/static/js/runtime-main.be6a7ef8.js"
+  }
+]);
